567f22f4f7f5c3e9266389e22f01c31efb8f1cbe
Ricardo Pereira <patresio@gmail.com>
2024-11-06 08:39:17 -0300

    chore: add prisma and database setup

---
+++ app/transactions/[id]/page.tsx
@@ -0,0 +1,5 @@
+const Transaction = ({ params: { id }}: {params}) => {
+  return <h1>Transaction {id}</h1>
+}
+
+export default Transaction
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const Transaction = () => {
+  return <h1>Transaction Page</h1>
+}
+
+export default Transaction
