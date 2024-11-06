0f9087009e100d074bf4bf3ae8be3c3a2f0f7b2c
Ricardo Pereira <patresio@gmail.com>
2024-11-06 10:48:20 -0300

    chore: git hooks setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
-import { UserButton } from '@clerk/nextjs'
-import { auth } from '@clerk/nextjs/server'
-import { dark } from '@clerk/themes'
-import { redirect } from 'next/navigation'
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const Home = () => {
-  const { userId } = auth()
+  const { userId } = auth();
   if (!userId) {
-    redirect('/login')
+    redirect("/login");
   }
   return (
     <div className="flex h-full items-center justify-center">
       <UserButton showName />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
