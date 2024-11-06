f23428045a2b6e64ea40f27b6988cab2c9214237
Ricardo Pereira <patresio@gmail.com>
2024-11-06 14:01:33 -0300

    chore: badge type transaction

---
+++ app/transactions/_components/type-badge.tsx
@@ -0,0 +1,37 @@
+import { Badge } from "@/app/_components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
+import { CircleIcon } from "lucide-react";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transaction;
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === TransactionType.DEPOSIT) {
+    return (
+      <Badge className="bg-muted text-primary font-bold hover:bg-muted">
+        <CircleIcon className="mr-2 fill-primary" size={8} />
+        Depósito
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.EXPENSE) {
+    return (
+      <Badge className="font-bold text-danger bg-danger bg-opacity-10 hover:bg-danger hover:bg-opacity-10">
+        <CircleIcon className="mr-2 fill-danger" size={8} />
+        Despesa
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.INVESTMENT) {
+    return (
+      <Badge className="font-bold text-white bg-white bg-opacity-10 hover:bg-white hover:bg-opacity-10">
+        <CircleIcon className="mr-2 fill-white" size={8} />
+        Investimento
+      </Badge>
+    );
+  }
+  return "Erro";
+};
+
+export default TransactionTypeBadge;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,24 @@
-const TransactionsPage = () => {
-  return <h1>Transactions Page</h1>
-}
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
 
-export default TransactionsPage
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div className="p-6 space-y-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar Transacão
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
+};
+
+export default TransactionsPage;
