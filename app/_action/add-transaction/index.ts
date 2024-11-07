f49d8d0b3955aec9c97a639917712eb968d88586
Ricardo Pereira <patresio@gmail.com>
2024-11-07 09:52:11 -0300

    chore: add and edit transaction db

--- app/_action/add-transaction/index.ts
+++ app/_action/add-transaction/index.ts
@@ -6,10 +6,11 @@ import {
   TransactionType,
   TranscationPaymentMethod,
 } from "@prisma/client";
-import { addTransactionSchema } from "./schema";
+import { upsertTransactionSchema } from "./schema";
 import { revalidatePath } from "next/cache";
 
-interface AddTransactionParams {
+interface upsertTransactionParams {
+  id?: string;
   name: string;
   amount: number;
   type: TransactionType;
@@ -18,13 +19,19 @@ interface AddTransactionParams {
   date: Date;
 }
 
-export const addTransaction = async (params: AddTransactionParams) => {
-  addTransactionSchema.parse(params);
+export const upsertTransaction = async (params: upsertTransactionParams) => {
+  upsertTransactionSchema.parse(params);
   const { userId } = await auth();
   if (!userId) {
     throw new Error("User not found");
   }
 
-  await db.transaction.create({ data: { ...params, userId } });
+  await db.transaction.upsert({
+    update: { ...params, userId },
+    create: { ...params, userId },
+    where: {
+      id: params?.id ?? "",
+    },
+  });
   revalidatePath("/transactions");
 };
