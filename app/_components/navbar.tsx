4c15ac827bc85dd5ade2e307a001e7dd8001b7c8
Ricardo Pereira <patresio@gmail.com>
2024-11-11 09:52:18 -0300

    feat: add navbar component

---
+++ app/_components/navbar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex items-center justify-between px-8 py-4 border-b border-solid">
+      <div className="flex items-center gap-10">
+        <Image src="/logo.svg" width={173} height={39} alt="Finance AI" />
+        <Link
+          href="/"
+          className={
+            pathname === "/"
+              ? "text-primary font-bold"
+              : "text-muted-foreground"
+          }
+        >
+          Dashboard
+        </Link>
+        <Link
+          href="/transactions"
+          className={
+            pathname === "/transactions"
+              ? "text-primary font-bold"
+              : "text-muted-foreground"
+          }
+        >
+          Transações
+        </Link>
+        <Link
+          href="/subscriptions"
+          className={
+            pathname === "/subscriptions"
+              ? "text-primary font-bold"
+              : "text-muted-foreground"
+          }
+        >
+          Assinatura
+        </Link>
+      </div>
+      <UserButton showName />
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
+import {} from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import Navbar from "./_components/navbar";
 
 const Home = () => {
   const { userId } = auth();
@@ -8,8 +9,8 @@ const Home = () => {
     redirect("/login");
   }
   return (
-    <div className="flex h-full items-center justify-center">
-      <UserButton showName />
+    <div className="flex h-full w-full">
+      <Navbar />
     </div>
   );
 };
