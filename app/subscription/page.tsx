e689b2452560f24f6664101d55e9876f3ac749f5
Ricardo Pereira <patresio@gmail.com>
2024-11-11 11:20:49 -0300

    feat: ensure transactions and subscriptions pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
+import Navbar from "../_components/navbar";
+import { redirect } from "next/navigation";
+
 const SubscriptionsPage = () => {
-    return (  );
-}
- 
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return <Navbar />;
+};
+
 export default SubscriptionsPage;
